a690ae84e3ed5fd250d70f04e90a3edbe4067ba5
Mustafa Yusuf <mustafa.yusuf@turkcell.com.tr>
2017-08-16 11:00:39 +0300

    targets seperated

---
+++ Mapper-iOS/Mapper_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Mapper_iOS.h
+//  Mapper-iOS
+//
+//  Created by Mustafa Yusuf on 16/08/2017.
+//  Copyright © 2017 Lyft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mapper_iOS.
+FOUNDATION_EXPORT double Mapper_iOSVersionNumber;
+
+//! Project version string for Mapper_iOS.
+FOUNDATION_EXPORT const unsigned char Mapper_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mapper_iOS/PublicHeader.h>
+
+
